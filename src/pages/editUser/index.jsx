a878f9da4c95f61df3bcbf3ff3033555729aa896
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2024-01-18 11:36:11 +0100

    feat: ajout de la page Edit User.

---
+++ src/pages/editUser/index.jsx
@@ -0,0 +1,71 @@
+import style from "./editUser.module.css"
+import Account from "../../components/account"
+import data from "../../mockedData/accountsData.json"
+import MainButton from "../../components/mainButton"
+
+
+const accountContent = data.accountsContent
+
+/**
+ * Return the edit user page
+ * @returns { HTMLElement }
+ */
+function EditUser() {
+    return (
+        <main className={`${style.main} bg-light`}>
+            <section className={style.edit}>
+                <h1>Welcome back</h1>
+                <form className={style.form}>
+                    <div className={style.input_wrapper_firstname}>
+                        <label htmlFor="firstname" className="sr-only">
+                            Firstname
+                        </label>
+                        <input
+                            type="text"
+                            id="firstname"
+                            name="firstname"
+                            defaultValue="Tony"
+                        />
+                    </div>
+                    <div className={style.input_wrapper_lastname}>
+                        <label htmlFor="lastname" className="sr-only">
+                            Lastname
+                        </label>
+                        <input type="text"
+                            id="lastname"
+                            name="lastname"
+                            defaultValue="Stark"
+                        />
+                    </div>
+                    <div className={style.save_button}>
+                        <MainButton
+                            text="Save"
+                            isLittleVersion={true}
+                        />
+                    </div>
+                    <div className={style.cancel_button}>
+                        <MainButton
+                            text="Cancel"
+                            isLittleVersion={true}
+                            navLink="/user"
+                        />
+                    </div>
+                </form>
+            </section>
+
+            <section className={style.accountsContainer}>
+                <h2 className="sr-only">Accounts</h2>
+                {accountContent.map(({ id, title, amount, description }) => (
+                    <Account
+                        key={id}
+                        title={title}
+                        amount={amount}
+                        description={description}
+                    />
+                ))}
+            </section>
+        </main>
+    )
+}
+
+export default EditUser
