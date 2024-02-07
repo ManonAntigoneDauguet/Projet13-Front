a2886a2efa85c910d2d6c66212381eda6be9b5dd
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2024-02-07 09:53:09 +0100

    refactor: basculement du try/catch du call API editUser() à changeInformations() dans la page Profile.

--- src/services/callAPI.service.jsx
+++ src/services/callAPI.service.jsx
@@ -29,24 +29,20 @@ async function getUser(token) {
 }
 
 async function editUser(token, firstName, lastName) {
-    try {
-        const response = await fetch(`http://localhost:3001/api/v1/user/profile`, {
-            method: "PUT",
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                "firstName": firstName,
-                "lastName": lastName
-            })
+    const response = await fetch(`http://localhost:3001/api/v1/user/profile`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            "firstName": firstName,
+            "lastName": lastName
         })
-        const user = await response.json()
-        console.log(user.message)
-        return user.body
-    } catch {
-        return null
-    }
+    })
+    const user = await response.json()
+    console.log(user.message)
+    return user.body
 }
 
 export { getUser, getToken, editUser }
